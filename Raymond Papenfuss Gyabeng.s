3ceae63f40491497eceb948453c66db0d3c7a3e1
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-10-16 23:15:21 -0400

    Add files via upload
    The objective of this Mips sample is to print out my name

---
+++ Raymond Papenfuss Gyabeng.s
@@ -0,0 +1,9 @@
+# Raymond Papenfuss Gyabeng.s
+#
+# This is to Print out My name
+#
+         .data
+msg:   .asciiz "Raymond Papenfuss Gyabeng"
+	.extern foobar 4
+         .text
+        .globl main
