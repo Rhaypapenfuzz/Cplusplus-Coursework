a6cb4df7bb58ba9862cad10bc3db8d74a98319f3
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-10-18 16:52:28 -0400

    Update Raymond Papenfuss Gyabeng.s
    Changed system call code from character to printing integer and perfomed additions to print zero and two

--- Raymond Papenfuss Gyabeng.s
+++ Raymond Papenfuss Gyabeng.s
@@ -1,12 +1,19 @@
-.data			# Assembly language instructions
+.data			#Data declaration section
 
 at_sign:	.word 64
 
-.text
+.text			# Assembly language instructions
 .globl main
 
 main: 			# My main
-    lw $a0, at_sign
-	  li $v0, 11
-	  syscall   # Exit program
+   	  lw $a0, at_sign
+	  li $v0, 11		#system call code for printing character
+	  syscall   		#Call Operating system to perform operation
+	  
+	  li $v0, 1		#system call code for printing integer
+	addi $a0, $a0, -64	#Addition to get integer 0
+	syscall
+	
+	addi $a0, $a0, 2	#Addition to get integer 2
+	syscall
 	  jr $ra		# return from main
